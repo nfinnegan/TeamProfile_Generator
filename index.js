c7a0a7d17522a977cf3a85f1816fdf97c981d1f9
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-07 16:41:50 -0600

    got half of employee test to pass

---
+++ index.js
@@ -0,0 +1,3 @@
+const Employee = require("./Lib/employee");
+
+let emp1 = new Employee("nat", 234, "some@gmail.com");
--- tests/employee.test.js
+++ tests/employee.test.js
@@ -4,40 +4,50 @@ describe("Employee", () => {
   describe("Initialization", () => {
     it("should instantiate a new Employee class", () => {
       const emp = new Employee();
-      expect(emp).toEqual({
-        name: "Natalie",
-        id: 1234,
-        email: "someEmail@gmail.com",
-      });
+
+      expect(typeof emp).toBe("object");
     });
-  });
-  describe("getName", () => {
+
     it("should set name based off user input from question prompt", () => {
-      const emp = new Employee(name);
       const name = "Natalie";
-      expect(emp).toEqual(name);
+      const emp = new Employee(name);
+
+      expect(emp.name).toEqual(name);
     });
-  });
-  describe("getID", () => {
     it("should set id based off user input from question prompt", () => {
-      const emp = new Employee(id);
       const id = 123;
-      expect(emp).toEqual(id);
+      const emp = new Employee("Natalie", id);
+
+      expect(emp.id).toEqual(id);
       //should I limit a max length for input?
     });
-  });
-  describe("getEmail", () => {
     it("should set email based off user input from question prompt", () => {
-      const emp = new Employee(email);
       const email = "yourEmail@gmail.com";
-      expect(emp).toEqual(email);
+      const emp = new Employee("Natalie", 234, email);
+
+      expect(emp.email).toEqual(email);
     });
   });
-  describe("getRole", () => {
-    it("should set email based off user input from question prompt", () => {
-      const role = "Employee";
-      const emp = new Employee("Natalie", 123, "someEmail@gmail.com");
-      expect(emp).toEqual(role);
-    });
-  });
+  //   describe("getID", () => {
+  //     it("should set id based off user input from question prompt", () => {
+  //       const emp = new Employee(id);
+  //       const id = 123;
+  //       expect(emp.id).toEqual(id);
+  //       //should I limit a max length for input?
+  //     });
+  //   });
+  //   describe("getEmail", () => {
+  //     it("should set email based off user input from question prompt", () => {
+  //       const emp = new Employee(email);
+  //       const email = "yourEmail@gmail.com";
+  //       expect(emp.email).toEqual(email);
+  //     });
+  //   });
+  //   describe("getRole", () => {
+  //     it("should set email based off user input from question prompt", () => {
+  //       const role = "Employee";
+  //       const emp = new Employee("Natalie", 123, "someEmail@gmail.com");
+  //       expect(emp).toEqual(role);
+  //     });
+  //   });
 });
