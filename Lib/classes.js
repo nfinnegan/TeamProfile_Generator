e0da7c2d322c134cb23e335fe41dac576f52b1e9
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-06 19:49:50 -0600

    created eng and intern subclasses

--- Lib/classes.js
+++ Lib/classes.js
@@ -23,3 +23,22 @@ class Manager extends Employee {
     // overridden to return 'Manager'
   }
 }
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGithub() {}
+  getRole() {} //overridden to return 'engineer'
+}
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+  getSchool() {}
+  getRole() {} //overridden to return 'intern'
+}
