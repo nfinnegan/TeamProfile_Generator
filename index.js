6028d5eb90c662558b0c1f989fd06147fd808be2
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-08 19:54:38 -0600

    cant figure out loop, broke out prompts per role

--- index.js
+++ index.js
@@ -6,64 +6,87 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
+newManager = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Please enter managers name",
+    },
+    {
+      type: "input",
+      name: "mgrID",
+      message: "Please enter your ID number",
+    },
+    {
+      type: "input",
+      name: "mgrEmail",
+      message: "Please enter your email",
+    },
+    {
+      type: "input",
+      name: "offNum",
+      message: "Please enter your office phone number",
+    },
+  ]);
+};
+
+newEngineer = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "engName",
+      message: "Please enter your engineers name",
+    },
+    {
+      type: "input",
+      name: "engID",
+      message: "Please enter your engineers ID number",
+    },
+    {
+      type: "input",
+      name: "engEmail",
+      message: "Please enter your engineers email",
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "Please enter your engineers GitHub username",
+    },
+  ]);
+};
+
+newIntern = () => {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "intName",
+      message: "Please enter your intern's name",
+    },
+    {
+      type: "input",
+      name: "intID",
+      message: "Please enter your interns ID number",
+    },
+    {
+      type: "input",
+      name: "intEmail",
+      message: "Please enter your interns email",
+    },
+    {
+      type: "input",
+      name: "intSchool",
+      message: "Please enter your interns school",
+    },
+  ]);
+};
+
 const questions = [
-  {
-    type: "input",
-    name: "managerName",
-    message: "Please enter managers name",
-  },
-  {
-    type: "input",
-    name: "mgrID",
-    message: "Please enter your ID number",
-  },
-  {
-    type: "input",
-    name: "mgrEmail",
-    message: "Please enter your email",
-  },
-  {
-    type: "input",
-    name: "offNum",
-    message: "Please enter your office phone number",
-  },
   {
     type: "list",
     name: "addTeam",
     message: "Would you like to add anyone to your team?",
     choices: ["Engineer", "Intern", "No one else to add"],
-  },
-  {
-    type: "input",
-    name: "engName",
-    message: "Please enter your engineers name",
-    when: function (answers) {
-      return answers.addTeam === "Engineer";
-    },
-  },
-  {
-    type: "input",
-    name: "engID",
-    message: "Please enter your engineers ID number",
-  },
-  {
-    type: "input",
-    name: "engEmail",
-    message: "Please enter your engineers email",
-  },
-  {
-    type: "input",
-    name: "gitHub",
-    message: "Please enter your engineers GitHub username",
-  },
-  {
-    type: "list",
-    name: "addTeam2",
-    message: "Would you like to add anyone else to your team?",
-    choices: ["Engineer", "Intern", "No one else to add"],
-    when: function (answers) {
-      return answers.gitHub === true;
-    },
   },
 ];
 
