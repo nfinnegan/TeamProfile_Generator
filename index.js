d72c21a57a2f0c611f2d09a53e91f18e8f0e2c26
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-08 20:45:01 -0600

    getting input data from inquirer prompt

--- index.js
+++ index.js
@@ -6,79 +6,108 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
+const allEmployees = [];
+
 newManager = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "managerName",
-      message: "Please enter managers name",
-    },
-    {
-      type: "input",
-      name: "mgrID",
-      message: "Please enter your ID number",
-    },
-    {
-      type: "input",
-      name: "mgrEmail",
-      message: "Please enter your email",
-    },
-    {
-      type: "input",
-      name: "offNum",
-      message: "Please enter your office phone number",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Please enter managers name",
+      },
+      {
+        type: "input",
+        name: "mgrID",
+        message: "Please enter your ID number",
+      },
+      {
+        type: "input",
+        name: "mgrEmail",
+        message: "Please enter your email",
+      },
+      {
+        type: "input",
+        name: "offNum",
+        message: "Please enter your office phone number",
+      },
+    ])
+    .then((answers) => {
+      const nManager = new Manager(
+        answers.managerName,
+        answers.mgrID,
+        answers.mgrEmail,
+        answers.offNum
+      );
+    });
 };
 
 newEngineer = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "engName",
-      message: "Please enter your engineers name",
-    },
-    {
-      type: "input",
-      name: "engID",
-      message: "Please enter your engineers ID number",
-    },
-    {
-      type: "input",
-      name: "engEmail",
-      message: "Please enter your engineers email",
-    },
-    {
-      type: "input",
-      name: "gitHub",
-      message: "Please enter your engineers GitHub username",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engName",
+        message: "Please enter your engineers name",
+      },
+      {
+        type: "input",
+        name: "engID",
+        message: "Please enter your engineers ID number",
+      },
+      {
+        type: "input",
+        name: "engEmail",
+        message: "Please enter your engineers email",
+      },
+      {
+        type: "input",
+        name: "gitHub",
+        message: "Please enter your engineers GitHub username",
+      },
+    ])
+    .then((answers) => {
+      const nEng = new Engineer(
+        answers.engName,
+        answers.engID,
+        answers.engEmail,
+        answers.gitHub
+      );
+    });
 };
 
 newIntern = () => {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "intName",
-      message: "Please enter your intern's name",
-    },
-    {
-      type: "input",
-      name: "intID",
-      message: "Please enter your interns ID number",
-    },
-    {
-      type: "input",
-      name: "intEmail",
-      message: "Please enter your interns email",
-    },
-    {
-      type: "input",
-      name: "intSchool",
-      message: "Please enter your interns school",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "intName",
+        message: "Please enter your intern's name",
+      },
+      {
+        type: "input",
+        name: "intID",
+        message: "Please enter your interns ID number",
+      },
+      {
+        type: "input",
+        name: "intEmail",
+        message: "Please enter your interns email",
+      },
+      {
+        type: "input",
+        name: "intSchool",
+        message: "Please enter your interns school",
+      },
+    ])
+    .then((answers) => {
+      const nIntern = new Intern(
+        answers.intName,
+        answers.intID,
+        answers.intEmail,
+        answers.intSchool
+      );
+    });
 };
 
 const questions = [
