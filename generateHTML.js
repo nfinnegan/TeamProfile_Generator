00c50aeac891ab60806775c162ba01a77252af02
Natalie Finnegan <nfinneg2@gmail.com>
2021-04-10 15:09:42 -0600

    cards generating to HTML with 4 sep Q populating

--- generateHTML.js
+++ generateHTML.js
@@ -1,73 +1,150 @@
 //how do I get the role added?
 
-//const Manager = require("./Lib/manager");
+const Manager = require("./Lib/manager");
 
-//     if (`${emp.officeNumber}`) {
-//       $("<li></li>")
-//         .addClass("list-group-item")
-//         .appendTo(empInfo)
-//         .text(`Office Num:${emp.officeNumber}`);
-//     } else if (`${emp.github}`) {
-//       $("<li></li>")
-//         .addClass("list-group-item")
-//         .appendTo(empInfo)
-//         .text(`Github:${emp.github}`);
-//     } else if (`${emp.school}`) {
-//       $("<li></li>")
-//         .addClass("list-group-item")
-//         .appendTo(empInfo)
-//         .text(`School:${emp.school}`);
-//     }
-//   });
-// };
-
-const generateHTML2 = (allEmployees) => {
-  console.log(allEmployees);
+const teamCards = [];
+const createCards = (allEmployees) => {
+  for (let i = 0; i < allEmployees.length; i++) {
+    if (allEmployees[i].getRole() === "Manager") {
+      teamCards.push(`<div class="card" style="width: 18rem">
+    <div class="card-header">Role: ${allEmployees[i].getRole()}</div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Name: ${allEmployees[i].name}</li>
+      <li class="list-group-item">Email: ${allEmployees[i].email}</li>
+      <li class="list-group-item">Emp ID: ${allEmployees[i].id}</li>
+      <li class="list-group-item">Emp Office #: ${
+        allEmployees[i].officeNumber
+      }</li>
+    </ul>
+    </div>`);
+    } else if (allEmployees[i].getRole() === "Engineer") {
+      teamCards.push(`<div class="card" style="width: 18rem">
+    <div class="card-header">Role: ${allEmployees[i].getRole()}</div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Name: ${allEmployees[i].name}</li>
+      <li class="list-group-item">Email: ${allEmployees[i].email}</li>
+      <li class="list-group-item">Emp ID: ${allEmployees[i].id}</li>
+      <li class="list-group-item">GitHub: ${allEmployees[i].github}</li>
+    </ul>
+    </div>`);
+    }
+  }
   let html = `<!DOCTYPE html>
-    <html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <title>My Team</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+        crossorigin="anonymous"
+      />
       <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>My Team</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
-          crossorigin="anonymous"
-        />
-        <head>
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        </head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       </head>
-      <body>
-        <header>
-          <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-              <h1 class="display-4 text-center bg-dark" style="color: white">
-                My Team
-              </h1>
-            </div>
+    </head>
+    <body>
+      <header>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4 text-center bg-dark" style="color: white">
+              My Team
+            </h1>
           </div>
-        </header>
-        <div class="container">`;
+        </div>
+      </header>
+      <div class="container">
+${teamCards}
+</div>
 
-  allEmployees.forEach((emp) => {
-    html += `   <div class="card" style="width: 18rem">
-<div class="card-header">Featured</div>
-<ul class="list-group list-group-flush">
-  <li class="list-group-item">Name: ${emp.name}</li>
-  <li class="list-group-item">Email: ${emp.email}</li>
-  <li class="list-group-item">Emp ID: ${emp.id}</li>
-</ul>
-</div>`;
-  });
-  html += `   </div>
-
-        
-      </body>
-    </html>`;
+    </body>
+  </html>`;
   return html;
 };
 
-module.exports = generateHTML2;
+// let html = `<!DOCTYPE html>
+//     <html>
+//       <head>
+//         <meta charset="utf-8" />
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//         <title>My Team</title>
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <link
+//           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+//           rel="stylesheet"
+//           integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+//           crossorigin="anonymous"
+//         />
+//         <head>
+//           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+//         </head>
+//       </head>
+//       <body>
+//         <header>
+//           <div class="jumbotron jumbotron-fluid">
+//             <div class="container">
+//               <h1 class="display-4 text-center bg-dark" style="color: white">
+//                 My Team
+//               </h1>
+//             </div>
+//           </div>
+//         </header>
+//         <div class="container">
+//   ${teamCards}
+//   </div>
+
+//       </body>
+//     </html>`;
+// return html;
+
+module.exports = createCards;
+
+// const generateHTML2 = (allEmployees) => {
+//   let html = `<!DOCTYPE html>
+//     <html>
+//       <head>
+//         <meta charset="utf-8" />
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//         <title>My Team</title>
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <link
+//           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+//           rel="stylesheet"
+//           integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+//           crossorigin="anonymous"
+//         />
+//         <head>
+//           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+//         </head>
+//       </head>
+//       <body>
+//         <header>
+//           <div class="jumbotron jumbotron-fluid">
+//             <div class="container">
+//               <h1 class="display-4 text-center bg-dark" style="color: white">
+//                 My Team
+//               </h1>
+//             </div>
+//           </div>
+//         </header>
+//         <div class="container">`;
+
+//   allEmployees.forEach((emp) => {
+//     html += `   <div class="card" style="width: 18rem">
+// <div class="card-header">Featured</div>
+// <ul class="list-group list-group-flush">
+//   <li class="list-group-item">Name: ${emp.name}</li>
+//   <li class="list-group-item">Email: ${emp.email}</li>
+//   <li class="list-group-item">Emp ID: ${emp.id}</li>
+// </ul>
+// </div>`;
+//   });
+//   html += `   </div>
+
+//       </body>
+//     </html>`;
+//   return html;
+// };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Manager = require("./Lib/manager");
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-const generateHTML = require("./generateHTML");
+const createCards = require("./generateHTML");
 
 const allEmployees = [];
 
@@ -67,7 +67,7 @@ newTeammatePrompt = () => {
       } else {
         console.log("team all built!", allEmployees);
         const fileName = "src/team.html";
-        writeToFile(fileName, generateHTML(allEmployees));
+        writeToFile(fileName, createCards(allEmployees));
       }
     });
 };
@@ -142,20 +142,3 @@ function init() {
 
 // Function call to initialize app
 init();
-
-module.exports = allEmployees;
-
-// addTeamMate = (role) => {
-//   switch (answers.choices) {
-//     case "Engineer":
-//       response = newEngineer();
-//       break;
-//     case "Intern":
-//       response = newIntern();
-//       break;
-//     case "No one else to add":
-//       response = "";
-//       break;
-//   }
-//   return answers[role];
-// };
